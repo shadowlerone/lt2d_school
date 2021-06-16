7d72b2496623898b189adedf5b32dc3b7cc5041d
shadowlerone <andrae.lewis@gmail.com>
2021-06-16 13:34:57 -0400

    moved CSS to its own files

---
+++ src/pages/meeting/dot.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="dot">
+		<a
+			:href="'https://meet.jit.si/' + meeting"
+			target="meeting_iframe"
+			onclick="breakout(this)"
+		>
+			<img type="image/svg+xml" src=src />
+		</a>
+	</div>
+</template>
+<script>
+
+
+export default {
+	props : ['src', 'meeting']
+}
+</script>
