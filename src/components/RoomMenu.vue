25e06bfeee39eeadc8d95d9f80746b21ed364ac7
shadowlerone <andrae.lewis@gmail.com>
2021-06-17 09:32:56 -0400

    pulled in menu stylings from Display-current-room

--- src/components/RoomMenu.vue
+++ src/components/RoomMenu.vue
@@ -1,41 +1,50 @@
 <template>
-	<div class="menu">
-		<ul>
-			<li class="room">
-				<div>
-					<input
-						type="text"
-						v-model="search"
-						v-on:keyup="room_filter"
-						placeholder="Search title.."
-						size="48"
-					/>
-				</div>
-			</li>
-			<li
-				v-for="item in filtered_rooms"
-				v-bind:key="item.id"
-				v-bind:name="item.name"
-				class="room"
-				onclick="closeMenu()"
-			>
-				<div
-					v-if="item.name === item.name.toUpperCase()"
-					class="program"
+	<div class="menu-container">
+		<div class="menu-backdrop"></div>
+		<div class="menu">
+			<ul>
+				<li class="room">
+					<div>
+						<input
+							type="text"
+							v-model="search"
+							v-on:keyup="room_filter"
+							placeholder="Search title.."
+							size="48"
+						/>
+					</div>
+				</li>
+				<li
+					v-for="item in filtered_rooms"
+					v-bind:key="item.id"
+					v-bind:name="item.name"
+					class="room"
+					onclick="closeMenu()"
 				>
-					{{ item.name }}
-				</div>
-				<div v-else class="roomLink">
-					<router-link
-						:to="{
-							name: item.route,
-							params: item.classroom
-						}"
-						>{{ item.name }}</router-link
+					<div
+						v-if="item.name === item.name.toUpperCase()"
+						class="program"
 					>
-				</div>
-			</li>
-		</ul>
+						{{ item.name }}
+					</div>
+					<div
+						v-else
+						class="room-link"
+						:class="{
+							current: $route.params.meet == item.classroom.meet
+						}"
+					>
+						<router-link
+							:to="{
+								name: item.route,
+								params: item.classroom
+							}"
+							>{{ item.name }}</router-link
+						>
+					</div>
+				</li>
+			</ul>
+		</div>
 	</div>
 </template>
 
@@ -75,6 +84,10 @@ export default {
 </script>
 
 <style>
+.menu-container > * {
+	/* transform: translate(-100%, 0);
+	transition: transform 0.5s cubic-bezier(0.77, 0.2, 0.05, 1); */
+}
 .menu,
 .menu * {
 	box-sizing: border-box;
@@ -90,15 +103,16 @@ export default {
 	overflow-x: hidden;
 	background-color: rgba(255, 255, 255, 0.8);
 	text-align: left;
-	z-index: 4;
+	z-index: 6;
 	transform-origin: 0% 0%;
-	/* transform: translate(-100%, 0);
-	transition: transform 0.5s cubic-bezier(0.77, 0.2, 0.05, 1); */
 }
 
 .menu li {
 	/* padding: 1rem; */
 	font-size: 1.1em;
+}
+.menu li:not(:last-child) {
+	margin-bottom: 1rem;
 }
 .menu li div {
 	padding: 0.5rem;
@@ -110,5 +124,39 @@ export default {
 	width: 100%;
 	/* padding-left: 0; */
 	padding: 1.2em;
+}
+.menu-backdrop {
+	width: 100vw;
+	height: 100vh;
+	z-index: 5;
+	top: 0;
+	position: fixed;
+	background-color: red;
+}
+
+.room-link {
+	cursor: pointer;
+	border-style: solid;
+	border-width: 2px;
+	border-color: black;
+	background-color: white;
+	padding: 0.5rem;
+	border-radius: 10px;
+	margin-left: 0.5rem;
+	margin-right: 0.5rem;
+}
+.room-link a {
+	color: black;
+	text-decoration: none;
+	display: block;
+	height: 100%;
+}
+.room-link:hover {
+	background-color: rgba(49, 200, 106, 1);
+	border-radius: 0;
+}
+.room-link.current {
+	border-radius: 0;
+	background-color: rgba(49, 200, 106, 1);
 }
 </style>
